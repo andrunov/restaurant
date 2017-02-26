64dcb1d0854a91169e0d10e35e66f481a517daa6
andrunov <andrunov@gmail.com>
2017-02-26 23:26:48 +0300

    Spring MVC

---
+++ src/main/java/ru/agorbunov/restaurant/web/RootController.java
@@ -0,0 +1,38 @@
+package ru.agorbunov.restaurant.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import ru.agorbunov.restaurant.service.UserService;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by Admin on 26.02.2017.
+ */
+@Controller
+public class RootController {
+
+    @Autowired
+    private UserService service;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String root() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public String users(Model model) {
+        model.addAttribute("users", service.getAll());
+        return "users";
+    }
+
+    @RequestMapping(value = "/users", method = RequestMethod.POST)
+    public String setUser(HttpServletRequest request) {
+        int userId = Integer.valueOf(request.getParameter("userId"));
+//        AuthorizedUser.setId(userId);
+        return "redirect:meals";
+    }
+}
