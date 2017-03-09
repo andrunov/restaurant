b0f01322b71f559c7c9c863edf93947ca6bab997
andrunov <andrunov@gmail.com>
2017-03-09 21:06:44 +0300

    Root controller refactoring2

--- src/main/java/ru/agorbunov/restaurant/web/RootController.java
+++ src/main/java/ru/agorbunov/restaurant/web/RootController.java
@@ -1,11 +1,7 @@
 package ru.agorbunov.restaurant.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import ru.agorbunov.restaurant.service.RestaurantService;
 
 /**
  * Created by Admin on 26.02.2017.
@@ -13,10 +9,8 @@ import ru.agorbunov.restaurant.service.RestaurantService;
 @Controller
 public class RootController {
 
-    @Autowired
-    private RestaurantService restaurantService;
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping(value = "/")
     public String root() {
         return "index";
     }
